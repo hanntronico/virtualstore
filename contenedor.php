49c5de5170a82bfc54ce0a91ddc45b97371111b8
hanntronico <pedro.bernal.84@gmail.com>
2020-07-12 04:38:56 -0500

    prueba para subir una actualizacion

--- contenedor.php
+++ contenedor.php
@@ -12,7 +12,7 @@ $res=@mysqli_query($link, $sql);
           <section id="land" class="py-5">
 
 
-            <div class="jumbotron">
+<!--             <div class="jumbotron">
               <h1 class="display-3">Hello, world!</h1>
               <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
               <hr class="my-4">
@@ -20,7 +20,7 @@ $res=@mysqli_query($link, $sql);
               <p class="lead">
                 <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
               </p>
-            </div>
+            </div> -->
 
             <?php //include 'slider/slider_gp.php'; ?>
 
